1d6d8a190facc57026f6862fa5e72a8ff1776810
Aravind Ganesh <AravindGanesh@users.noreply.github.com>
2017-04-25 16:19:36 +0530

    Testbench code, and fix readme  (#5)
    * Fibonacci Ring oscillator - code doesn't work since it's an infinite loop.
    
    * Fibonacci Ring Oscillator
    
    * Instructions for compiling and running vhdl code
    
    * Instructions for GTKWave
    
    * tried something
    
    * Merge dammit
    
    * Starting testbench code
    
    * Tried testbench code
    
    * fix README
    
    * fix README

---
+++ src/12c_tb.vhdl
@@ -0,0 +1,56 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity i2c_tb is
+end i2c_tb;
+
+architecture arch of i2c_tb is
+	constant T : time := 1 us;
+
+	component i2c is
+		port(
+
+			clk : in std_logic := '1';
+			ack_one : in std_logic;
+			ack_two : in std_logic;
+
+
+			data_to_master : in std_logic_vector(7 downto 0) ;
+			data_recieved_at_slave : out std_logic_vector(7 downto 0)
+
+			);
+	end component; -- arch
+
+	signal clk, ack_one, ack_two : std_logic;
+	signal data_to_master, data_recieved_at_slave : std_logic_vector(7 downto 0);
+
+	begin
+		mapping : i2c port map(clk => clk, ack_one => ack_one, ack_two => ack_two,
+
+		data_to_master => data_to_master, data_recieved_at_slave => data_recieved_at_slave);
+
+		process 
+			clk <= '0';
+			wait for T/2;
+			clk <= '1';
+			wait for T/2;
+		end process;
+
+		process 
+			type pattern_type is record 
+				data_to_master, data_recieved_at_slave : std_logic_vector;
+				clk, ack_two, ack_one : std_logic;
+			end record;
+
+			type pattern_array is array (natural range <>) of pattern_type;
+			constant patterns : pattern_array :=
+					
+		end process;
+
+
+
+
+
+end arch;
+
+ 
