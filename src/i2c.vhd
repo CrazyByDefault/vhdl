200320b04717dd9ecf6384eeec9f561550b66e51
CrazyByDefault <es16btech11025@iith.ac.in>
2017-04-25 20:52:45 +0530

    Fix everything

--- src/i2c.vhd
+++ src/i2c.vhd
@@ -35,8 +35,8 @@ entity i2c is
     signal ack_from_slave : std_logic := '1';
 
     --Medium
-    signal sda : std_logic := '1';
-    signal scl : std_logic := '1';
+    --signal sda : std_logic := '1';
+    --signal scl : std_logic := '1';
 
     --Slave
     signal addr_reg : std_logic_vector(6 downto 0) := (others => '0');
@@ -44,9 +44,9 @@ entity i2c is
   
   begin
     
-    process (clk) is
+    process (scl) is
     begin
-      if rising_edge(clk) then
+      if rising_edge(scl) then
 
 
         case state_reg is
@@ -59,7 +59,7 @@ entity i2c is
 
 
           when start_addr_tx =>
-            if rising_edge(clk) then
+            if rising_edge(scl) then
               if bit_cnt < 7 then
                 bit_cnt <= bit_cnt + 1;
                 addr_reg(6 - bit_cnt) <= sda;
@@ -69,31 +69,31 @@ entity i2c is
                 end if;
               end if;
 
-              if bit_cnt = 8 and falling_edge(clk) then
+              if bit_cnt = 8 and falling_edge(scl) then
                 bit_cnt <= 0;
                 state_reg <= slave_hit_addr;
               end if;
+
 
 
           when slave_hit_addr =>
             --if addr_reg = addr_to_send_to then
              ack_from_slave <= ack_one;
             --end if;
-
-
-
-            if falling_edge(clk) and ack_from_slave = '0' then
+            if falling_edge(scl) and ack_from_slave = '0' then
               if rw_bit = '0' then
                 state_reg <= write;
               else
                 state_reg <= read;
-            else 
-            	assert false
-            	  report ("no ack after addr, switching to idle")
-            	  severity note;
-            	state_reg <= idle;
               end if;
+            else 
+              assert false
+                report ("no ack after addr, switching to idle")
+                severity note;
+              state_reg <= idle;
             end if;
+
+
 
           when slave_hit_data =>
             ack_from_slave <= ack_two;
@@ -104,6 +104,7 @@ entity i2c is
             	  report ("no ack after data, switching to idle")
             	  severity note;
             	state_reg <= idle;
+            end if;
 
 
           when done =>
@@ -111,14 +112,14 @@ entity i2c is
             
 
           when write =>
-            if rising_edge(clk) then
+            if rising_edge(scl) then
               bit_cnt <= bit_cnt + 1;
               if bit_cnt < 8 then
                 data_storage(7 - bit_cnt) <= sda;
               end if;
             end if;
 
-            if falling_edge(clk) and bit_cnt = 8 then
+            if falling_edge(scl) and bit_cnt = 8 then
               state_reg <= slave_hit_data;
               bit_cnt <= 0;
             end if;
@@ -139,11 +140,6 @@ entity i2c is
         end if;
 
       end if;
-    end process;
-
-
-
-
-                    
+    end process;                    
   
   end architecture arch; -- arch
